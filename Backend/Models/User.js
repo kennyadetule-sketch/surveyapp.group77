3a78364b09d7cf968b41c9e5b99bb36925712d52
Emmanuel Tom <emmanueltom5370@gmail.com>
2026-09-25 15:13:57 +0100

    Models created

--- Backend/Models/User.js
+++ Backend/Models/User.js
@@ -1,3 +1,40 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please add a valid email",
+      ],
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    profileImage: {
+      type: String,
+      default: "",
+    },
+    role: {
+      type: String,
+      enum: ["creator", "admin"],
+      default: "creator",
+    },
+  },
 
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("User", userSchema);
